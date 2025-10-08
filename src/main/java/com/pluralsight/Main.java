8f9e6b8c27c13e866f7fb64da9ac9e87fb2feeff
hunbalkhan <hunbal4@gmail.com>
2025-10-08 11:43:48 -0400

    Completed the first step, now moving onto the first bonus step after this.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,31 @@
 package com.pluralsight;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // This gets the inventory.
+        ArrayList<Product> inventory = getInventory();
+
+        // this displays all the prodyucts
+        System.out.println("Search inventory");
+        for (Product product : inventory) {
+            System.out.println("ID: " + product.getId() + " | Name: " + product.getName() + " | Price: $" + product.getPrice());
+        }
+    }
+
+    public static ArrayList<Product> getInventory() {
+        ArrayList<Product> products = new ArrayList<>();
+
+        // Manually adding products into the array list.
+        products.add(new Product( 1, "Apple                     ", 0.99));
+        products.add(new Product( 2, "Milk                      ", 2.59));
+        products.add(new Product( 3, "Eggs                      ", 2.89));
+        products.add(new Product( 4, "Chocolate Milk            ", 7.99));
+        products.add(new Product( 5, "Gold Standard Whey Protein", 59.99));
+
+        return products;
     }
 }
